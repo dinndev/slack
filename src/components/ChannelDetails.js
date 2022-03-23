029ea9ba26ae787e43133ba56a3732451ba7b8b9
Din <dinndev@gmail.com>
2022-03-23 09:28:59 +0800

    :sparkles: Now, members from a channell is visible on channel details.
    ..

--- src/components/ChannelDetails.js
+++ src/components/ChannelDetails.js
@@ -9,17 +9,28 @@ const ChannelDetails = () => {
     channelDetailsData.id && channel_members.map(({ user_id }) => user_id);
   const hostName =
     users.length > 0 ? users.filter(({ id }) => id === owner_id)[0]?.email : "";
-  console.log(channelMembersID);
-  console.log(users);
+  const channelMembers =
+    users.length > 0 && typeof channelMembersID !== "undefined"
+      ? users.filter(({ id }) => channelMembersID.includes(id))
+      : [];
+
   return (
     <div className="bg-primary flex flex-col items-center grow-4 p-5 h-screen w-44">
       <h2 className="text-md text-gray-300">Channel details</h2>
       <div className="rounded-xl items-center flex flex-col p-5 bg-gray-700 text-center w-full mt-10 h-full">
         <h1 className="text-xl font-bold">{name}</h1>
         <div className="flex mt-5 items-center ">
-          <p className=" mr-1 text-xs font-bold">host:</p>
-          <p className=" text-xs">{hostName && hostName}</p>
+          <p className=" text-xs"> {hostName && hostName}</p>
         </div>
+        <ul className="flex flex-col mt-10 ">
+          <h1 className="font-bold text-md mb-5"> Channel members</h1>
+          {channelMembers.length > 0 &&
+            channelMembers.map(({ email, id }) => (
+              <li className="text-xs my-1" key={id}>
+                {email}
+              </li>
+            ))}
+        </ul>
       </div>
     </div>
   );
